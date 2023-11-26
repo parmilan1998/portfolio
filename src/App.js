0596af3e4272bd60851a7174e262f6fb4e523dc6
Parmilan <parmilancs2024@gmail.com>
2023-11-26 21:18:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import './App.css'
+import { Aos } from 'aos'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
+  useEffect(() => {
+    // Aos.init()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='w-full h-[80px] leading-10 flex items-center'>
+      <div className='container'>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-3'>
+            <span className='h-[35px] w-[35px] bg-primaryColor text-white font-semibold text-lg flex justify-center items-center rounded-full '>
+              M
+            </span>
+            <div className='leading-6'>
+              <h2 className='text-xl text-primaryColor font-bold'>Parmilan</h2>
+              <p className='text-sm text-primaryColor font-semibold'>
+                Personal
+              </p>
+            </div>
+          </div>
+          <div className='menu'>
+            <ul className='flex items-center gap-10'>
+              <li>
+                <a className='text-headingColor font-semibold' href='#about'>
+                  About
+                </a>
+              </li>
+              <li>
+                <a className='text-headingColor font-semibold' href='#services'>
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  className='text-headingColor font-semibold'
+                  href='#portfolio'
+                >
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a className='text-headingColor font-semibold' href='#contact'>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className='flex items-center gap-4'>
+            <button className='flex items-center gap-2 text-primaryColor border border-solid border-primaryColor font-semibold max-h-10 py-2 px-4 rounded-lg hover:bg-primaryColor hover:text-white hover:font-semibold ease-in duration-300'>
+              <i class='ri-send-plane-line'></i>Let's Talk
+            </button>
+            <span className='text-2xl text-primaryColor md:hidden cursor-pointer'>
+              <i class='ri-menu-fold-fill'></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
